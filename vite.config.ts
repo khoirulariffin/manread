7d8f50ffdbca65a80ee410904cbd572f08724b59
khoirulariffin <khoirul.ariffin@ntxsolution.com>
2025-04-14 23:44:34 +0700

    delete base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   homepage: "https://khoirulariffin.github.io/manread/",
-  base: "/manread/",
+  // base: "/manread/",
   server: {
     host: "::",
     port: 8080,
