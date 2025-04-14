80780aa86fb9c1cc36b903e1ecb340dc76c05eed
khoirulariffin <khoirul.ariffin@ntxsolution.com>
2025-04-14 23:30:52 +0700

    add yml

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  homepage: "https://github.com/khoirulariffin/manread/",
-  // base: "/manread/",
+  base: "/manread/",
   server: {
     host: "::",
     port: 8080,
